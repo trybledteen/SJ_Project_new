b352bcce815a9d674d53548dfb1fd17e99c68267
trybledteen <vikaoleynik202006@gmail.com>
2024-05-09 20:07:19 +0200

    Header and Footer

--- contacts.php
+++ contacts.php
@@ -1,5 +1,5 @@
 <?php
-include('partials/header.php');
+include_once('partials/header.php');
 ?>    
 
     <main>
@@ -63,25 +63,6 @@ include('partials/header.php');
             </div>
         </section>
     </main>
-    <footer class="container bg-dark text-white">
-        <div class="row">
-          <div class="col-25">
-            <h3>Social network</h3>
-            <a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-telegram fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
-          </div>
-          <div class="col-25">
-            <h3>Rýchle odkazy</h3>
-            <p><a href="index.html">Home</a></p>
-            <p><a href="gallery.html">Gallery</a></p>
-            <p><a href="contacts.html">Contacts</a></p>
-          </div>
-        <div class="row">
-          Created by Viktoriia Oliynyk 
-        </div>
-    </footer>
-    <script src="js/menu.js"></script>
-    <script src="js/slider.js"></script>
-</body>
-</html>
+    <?php
+    include_once('partials/footer.php')
+  ?> 
--- gallery.php
+++ gallery.php
@@ -1,6 +1,6 @@
 <?php
-include('partials/header.php');
-?>    
+include_once('partials/header.php');
+?>     
     <main>
         <section class="slides-container">
             <div class="slide fade">
@@ -52,25 +52,6 @@ include('partials/header.php');
             </div>
             </section>
     </main>
-    <footer class="container bg-dark text-white">
-        <div class="row">
-          <div class="col-25">
-            <h3>Social network</h3>
-            <a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-telegram fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
-          </div>
-          <div class="col-25">
-            <h3>Rýchle odkazy</h3>
-            <p><a href="index.html">Home</a></p>
-            <p><a href="gallery.html">Gallery</a></p>
-            <p><a href="contacts.html">Contacts</a></p>
-          </div>
-        <div class="row">
-          Created by Viktoriia Oliynyk 
-        </div>
-      </footer>
-    <script src="js/menu.js"></script>
-    <script src="js/slider.js"></script>
-</body>
-</html>
+    <?php
+    include_once('partials/footer.php')
+  ?> 
